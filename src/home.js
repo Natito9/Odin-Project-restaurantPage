7b10c70abb3946aada8bcb13c79a36472249c2fd
Natito9 <179492180+Natito9@users.noreply.github.com>
2024-10-28 12:05:25 +0100

    imported css

--- src/home.js
+++
@@ -1,9 +0,0 @@
-function loadHome() {
-    const content = document.getElementById('content');
-    const heading = document.createElement('h1');
-    heading.textContent = 'Welcome to Our Restaurant';
-    content.appendChild(heading);
-  }
-  
-  export default loadHome;
-  
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import './styles.css';
-import loadHome from './home.js';
-import loadMenu from './menu.js';
-import loadContact from './contact.js';
+console.log("hello");
+import './style.css';
 
-function loadPage() {
-  const content = document.getElementById('content');
-  content.innerHTML = ''; // Clear previous content
-  loadHome(); // Load home page by default
-}
 
-document.addEventListener('DOMContentLoaded', loadPage);
+
+
+
